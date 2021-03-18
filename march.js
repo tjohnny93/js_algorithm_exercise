4b4cd68e4b5af7ff2dad0f3e9693a51757374421
Johnny Tae <tjohnny93@berkeley.edu>
2021-03-18 14:38:23 +0900

    March 18 #3 my first try

--- march.js
+++ march.js
@@ -115,6 +115,52 @@ var findErrorNums = function (nums) {
   return [x, y];
 };
 
-console.log(findErrorNums([1, 2, 2, 4]));
-console.log(findErrorNums([1, 1]));
-console.log(findErrorNums([1, 2, 3, 3, 4, 5, 7]));
+// console.log(findErrorNums([1, 2, 2, 4]));
+// console.log(findErrorNums([1, 1]));
+// console.log(findErrorNums([1, 2, 3, 3, 4, 5, 7]));
+
+/*
+
+March 18, 2021  #3
+
+Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+Follow up: Could you implement a solution using only O(1) extra space complexity and O(n) runtime complexity?
+
+Example 1:
+
+Input: nums = [3,0,1]
+Output: 2
+Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+Example 2:
+
+Input: nums = [0,1]
+Output: 2
+Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
+Example 3:
+
+Input: nums = [9,6,4,2,3,5,7,0,1]
+Output: 8
+Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
+Example 4:
+
+Input: nums = [0]
+Output: 1
+Explanation: n = 1 since there is 1 number, so all numbers are in the range [0,1]. 1 is the missing number in the range since it does not appear in nums.
+
+*/
+
+const missingNumber = (nums) => {
+  let sum = 0,
+    total = 0;
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+    total += i + 1;
+  }
+  return total - sum;
+};
+
+console.log(missingNumber([3, 0, 1]));
+console.log(missingNumber([0, 1]));
+console.log(missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]));
+console.log(missingNumber([0]));
