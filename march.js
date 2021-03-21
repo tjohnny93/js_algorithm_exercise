b95983da40d64db90256c9ab89da6ba4a150a352
Johnny Tae <tjohnny93@berkeley.edu>
2021-03-21 13:25:53 +0900

    March 21 #6-1 solved

--- march.js
+++ march.js
@@ -271,6 +271,72 @@ var kidsWithCandies = function (candies, extraCandies) {
   });
 };
 
-console.log(kidsWithCandies([2, 3, 5, 1, 3], 3));
-console.log(kidsWithCandies([4, 2, 1, 1, 2], 1));
-console.log(kidsWithCandies([12, 1, 12], 10));
+// console.log(kidsWithCandies([2, 3, 5, 1, 3], 3));
+// console.log(kidsWithCandies([4, 2, 1, 1, 2], 1));
+// console.log(kidsWithCandies([12, 1, 12], 10));
+
+/*
+March 21, 2021  #6-1
+
+You are given an m x n integer grid accounts where accounts[i][j] is the amount of money the i​​​​​​​​​​​th​​​​ customer has in the j​​​​​​​​​​​th​​​​ bank. Return the wealth that the richest customer has.
+
+A customer's wealth is the amount of money they have in all their bank accounts. The richest customer is the customer that has the maximum wealth.
+
+Input: accounts = [[1,2,3],[3,2,1]]
+Output: 6
+Explanation:
+1st customer has wealth = 1 + 2 + 3 = 6
+2nd customer has wealth = 3 + 2 + 1 = 6
+Both customers are considered the richest with a wealth of 6 each, so return 6.
+
+Input: accounts = [[1,5],[7,3],[3,5]]
+Output: 10
+Explanation: 
+1st customer has wealth = 6
+2nd customer has wealth = 10 
+3rd customer has wealth = 8
+The 2nd customer is the richest with a wealth of 10.
+
+Input: accounts = [[2,8,7],[7,1,3],[1,9,5]]
+Output: 17
+
+*/
+
+var maximumWealth = function (accounts) {
+  let max = 0;
+  for (let i = 0; i < accounts.length; i++) {
+    // let cur = accounts[i].reduce((a, b) => a + b, 0);
+    // cur > max ? (max = cur) : max;
+    sum(accounts[i]) > max ? (max = sum(accounts[i])) : max;
+  }
+  return max;
+
+  function sum(arr) {
+    let result = 0;
+    for (let i = 0; i < arr.length; i++) {
+      result += arr[i];
+    }
+    return result;
+  }
+};
+
+console.log(
+  maximumWealth([
+    [1, 2, 3],
+    [3, 2, 1],
+  ])
+);
+console.log(
+  maximumWealth([
+    [1, 5],
+    [7, 3],
+    [3, 5],
+  ])
+);
+console.log(
+  maximumWealth([
+    [2, 8, 7],
+    [7, 1, 3],
+    [1, 9, 5],
+  ])
+);
