679e025447db33115f59b2508702c829b08b8a8a
Johnny Tae <tjohnny93@berkeley.edu>
2021-03-23 21:25:18 +0900

    March 23 #8-1 solved

--- march.js
+++ march.js
@@ -449,5 +449,43 @@ var numJewelsInStones = function (jewels, stones) {
   return count;
 };
 
-console.log(numJewelsInStones("aA", "aAAbbbb"));
-console.log(numJewelsInStones("z", "ZZ"));
+// console.log(numJewelsInStones("aA", "aAAbbbb"));
+// console.log(numJewelsInStones("z", "ZZ"));
+
+/*
+
+March 23, 2021  #8-1
+
+Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it. That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+
+Return the answer in an array.
+
+Example 1:
+
+Input: nums = [8,1,2,2,3]
+Output: [4,0,1,1,3]
+
+Example 2:
+
+Input: nums = [6,5,4,8]
+Output: [2,1,0,3]
+Example 3:
+
+Input: nums = [7,7,7,7]
+Output: [0,0,0,0]
+
+*/
+
+var smallerNumbersThanCurrent = function (nums) {
+  let result = new Array(nums.length).fill(0);
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      if (nums[i] > nums[j]) result[i] += 1;
+    }
+  }
+  return result;
+};
+
+console.log(smallerNumbersThanCurrent([8, 1, 2, 2, 3]));
+console.log(smallerNumbersThanCurrent([6, 5, 4, 8]));
+console.log(smallerNumbersThanCurrent([7, 7, 7, 7]));
