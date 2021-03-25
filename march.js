62db8353aeb5f43c9e83f98c3597add256742f51
Johnny Tae <tjohnny93@berkeley.edu>
2021-03-25 20:26:02 +0900

    March 25 #10 solved

--- march.js
+++ march.js
@@ -573,8 +573,47 @@ var restoreString = function (s, indices) {
   return result.join("");
 };
 
-console.log(restoreString("codeleet", [4, 5, 6, 7, 0, 2, 1, 3]));
-console.log(restoreString("abc", [0, 1, 2]));
-console.log(restoreString("aiohn", [3, 1, 4, 2, 0]));
-console.log(restoreString("aaiougrt", [4, 0, 2, 6, 7, 3, 1, 5]));
-console.log(restoreString("art", [1, 0, 2]));
+// console.log(restoreString("codeleet", [4, 5, 6, 7, 0, 2, 1, 3]));
+// console.log(restoreString("abc", [0, 1, 2]));
+// console.log(restoreString("aiohn", [3, 1, 4, 2, 0]));
+// console.log(restoreString("aaiougrt", [4, 0, 2, 6, 7, 3, 1, 5]));
+// console.log(restoreString("art", [1, 0, 2]));
+
+/*
+
+March 25 #10
+
+Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+ 
+
+Example 1:
+
+Input: n = 234
+Output: 15 
+Explanation: 
+Product of digits = 2 * 3 * 4 = 24 
+Sum of digits = 2 + 3 + 4 = 9 
+Result = 24 - 9 = 15
+Example 2:
+
+Input: n = 4421
+Output: 21
+Explanation: 
+Product of digits = 4 * 4 * 2 * 1 = 32 
+Sum of digits = 4 + 4 + 2 + 1 = 11 
+Result = 32 - 11 = 21
+*/
+
+var subtractProductAndSum = function (n) {
+  let str = n.toString();
+  let product = 1;
+  let sum = 0;
+  for (let i = 0; i < str.length; i++) {
+    product *= Number(str[i]);
+    sum += Number(str[i]);
+  }
+  return product - sum;
+};
+
+console.log(subtractProductAndSum(234));
+console.log(subtractProductAndSum(4421));
