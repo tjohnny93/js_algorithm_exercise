08aa47977e030e6bb723303fda8cea768b4c4c3e
Johnny Tae <tjohnny93@berkeley.edu>
2021-03-25 20:35:28 +0900

    March 25 #10-2 solved

--- march.js
+++ march.js
@@ -581,7 +581,7 @@ var restoreString = function (s, indices) {
 
 /*
 
-March 25 #10
+March 25 #10-1
 
 Given an integer number n, return the difference between the product of its digits and the sum of its digits.
  
@@ -615,5 +615,47 @@ var subtractProductAndSum = function (n) {
   return product - sum;
 };
 
-console.log(subtractProductAndSum(234));
-console.log(subtractProductAndSum(4421));
+// console.log(subtractProductAndSum(234));
+// console.log(subtractProductAndSum(4421));
+
+/*
+
+March 25 #10-2
+
+You are given an array items, where each items[i] = [typei, colori, namei] describes the type, color, and name of the ith item. You are also given a rule represented by two strings, ruleKey and ruleValue.
+
+The ith item is said to match the rule if one of the following is true:
+
+ruleKey == "type" and ruleValue == typei.
+ruleKey == "color" and ruleValue == colori.
+ruleKey == "name" and ruleValue == namei.
+Return the number of items that match the given rule.
+
+ 
+
+Example 1:
+
+Input: items = [["phone","blue","pixel"],["computer","silver","lenovo"],["phone","gold","iphone"]], ruleKey = "color", ruleValue = "silver"
+Output: 1
+Explanation: There is only one item matching the given rule, which is ["computer","silver","lenovo"].
+Example 2:
+
+Input: items = [["phone","blue","pixel"],["computer","silver","phone"],["phone","gold","iphone"]], ruleKey = "type", ruleValue = "phone"
+Output: 2
+Explanation: There are only two items matching the given rule, which are ["phone","blue","pixel"] and ["phone","gold","iphone"]. Note that the item ["computer","silver","phone"] does not match.
+*/
+
+var countMatches = function (items, ruleKey, ruleValue) {
+  let keys = {
+    type: 0,
+    color: 1,
+    name: 2,
+  };
+
+  let count = 0;
+
+  for (let i = 0; i < items.length; i++) {
+    if (items[i][keys[ruleKey]] === ruleValue) count += 1;
+  }
+  return count;
+};
