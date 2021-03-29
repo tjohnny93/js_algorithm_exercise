f1930a23ab71be6aef8a749d64dbe4e10a643ad2
Johnny Tae <tjohnny93@berkeley.edu>
2021-03-29 23:19:37 +0900

    March 29 #13 Solved

--- march.js
+++ march.js
@@ -739,3 +739,36 @@ var interpret = function (command) {
   }
   return result;
 };
+
+/* March 29 #13
+Given two arrays of integers nums and index. Your task is to create target array under the following rules:
+
+Initially target array is empty.
+From left to right read nums[i] and index[i], insert at index index[i] the value nums[i] in target array.
+Repeat the previous step until there are no elements to read in nums and index.
+Return the target array.
+
+It is guaranteed that the insertion operations will be valid.
+
+Example 1:
+
+Input: nums = [0,1,2,3,4], index = [0,1,2,2,1]
+Output: [0,4,1,3,2]
+
+Example 2:
+
+Input: nums = [1,2,3,4,0], index = [0,1,2,3,0]
+Output: [0,1,2,3,4]
+
+Input: nums = [1], index = [0]
+Output: [1]
+
+*/
+
+var createTargetArray = function (nums, index) {
+  let result = [];
+  for (let i = 0; i < nums.length; i++) {
+    result.splice(index[i], 0, nums[i]);
+  }
+  return result;
+};
