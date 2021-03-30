ff13d8a45779491fc3b5d463039cca37751ceba4
Johnny Tae <tjohnny93@berkeley.edu>
2021-03-30 19:46:43 +0900

    March 30 #14 solved

--- march.js
+++ march.js
@@ -805,3 +805,26 @@ var balancedStringSplit = function (s) {
   }
   return count;
 };
+
+/* March 30 #14
+You are given a string allowed consisting of distinct characters and an array of strings words. A string is consistent if all characters in the string appear in the string allowed.
+Return the number of consistent strings in the array words.
+
+Input: allowed = "ab", words = ["ad","bd","aaab","baa","badab"]
+Output: 2
+
+Input: allowed = "abc", words = ["a","b","c","ab","ac","bc","abc"]
+Output: 7
+
+Input: allowed = "cad", words = ["cc","acd","b","ba","bac","bad","ac","d"]
+Output: 4
+*/
+
+var countConsistentStrings = function (allowed, words) {
+  return words.filter((word) => {
+    for (let i = 0; i < word.length; i++) {
+      if (!allowed.includes(word[i])) return false;
+    }
+    return true;
+  }).length;
+};
