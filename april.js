8f146cf07daeac73ccee1e6e47ae44380d39608f
Johnny Tae <tjohnny93@berkeley.edu>
2021-04-03 16:48:05 +0900

    April 3 # 3 solved

--- april.js
+++ april.js
@@ -99,3 +99,42 @@ var sumOddLengthSubarrays = function (arr) {
   }
   return result;
 };
+
+/* April 3 # 3
+You are given an integer array nums. The unique elements of an array are the elements that appear exactly once in the array.
+
+Return the sum of all the unique elements of nums.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,2]
+Output: 4
+Explanation: The unique elements are [1,3], and the sum is 4.
+Example 2:
+
+Input: nums = [1,1,1,1,1]
+Output: 0
+Explanation: There are no unique elements, and the sum is 0.
+Example 3:
+
+Input: nums = [1,2,3,4,5]
+Output: 15
+Explanation: The unique elements are [1,2,3,4,5], and the sum is 15.
+*/
+
+var sumOfUnique = function (nums) {
+  let result = 0;
+  let count = {};
+  for (let i = 0; i < nums.length; i++) {
+    count[nums[i]] ? (count[nums[i]] += 1) : (count[nums[i]] = 1);
+  }
+  let countEntries = Object.entries(count);
+  for (let j = 0; j < countEntries.length; j++) {
+    if (countEntries[j][1] === 1) {
+      result += +countEntries[j][0];
+    }
+  }
+  return result;
+};
