e28612b194e1c3eb2e7d257708a504dbc5e895be
Johnny Tae <tjohnny93@berkeley.edu>
2021-04-12 14:30:35 +0900

    bubble, selection, insertion sort

--- sorts.js
+++ sorts.js
@@ -1,5 +1,7 @@
 let arr = [1, 5, 6, 8, 7, 2, 3, 4];
 
+// bubbleSort: compare each pair then move larger value to the back
+// since its nested time complexity will be O(n^2)
 const bubbleSort = (nums) => {
   let sorted = true; // ++ optimization to skip unneceesary looping (after everything is sorted);
   for (let i = nums.length; i > 0; i--) {
@@ -18,6 +20,9 @@ const bubbleSort = (nums) => {
 
 // console.log(bubbleSort(arr));
 
+// selectionSort: find the lowest value from each loop then place lowest value in order
+// time complexity: O(n^2)
+// selectionSort spend less time of swapping compare to bubbleSort since essentially bubbleSort would compare each pair and make swap everytime whereas selectionSort will make swap on end of each complete iteration
 const selectionSort = (nums) => {
   for (let i = 0; i < nums.length; i++) {
     let lowest = i;
@@ -29,4 +34,25 @@ const selectionSort = (nums) => {
   return nums;
 };
 
-console.log(selectionSort(arr));
+// console.log(selectionSort(arr));
+
+// insertionSort: check every item and based on the result of comparison place it in the right position
+// time complexity: O(n^2);
+// performance is good when array is already almost sorted
+
+const insertionSort = (nums) => {
+  for (let i = 1; i < nums.length; i++) {
+    let currentVal = nums[i];
+    let j = i - 1;
+    while (j >= 0 && nums[j] > currentVal) {
+      nums[j + 1] = nums[j];
+      j--;
+    }
+    nums[j + 1] = currentVal;
+  }
+  return nums;
+};
+
+console.log(insertionSort(arr));
+
+// RECAP: bubble, selection, and insertion are all fairly similar O(n^2), on Almost already sorted situation insertion O(n) > bubble O(n) > selection O(n^2);
