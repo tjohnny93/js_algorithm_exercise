3371ac38d3177c4f82f6bac9c93bb1364d32bdfb
Johnny Tae <tjohnny93@berkeley.edu>
2021-04-19 17:19:41 +0900

    April 19th heightChecker solved

--- april.js
+++ april.js
@@ -197,10 +197,53 @@ var uniqueOccurrences = function (arr) {
     count[arr[i]] ? (count[arr[i]] += 1) : (count[arr[i]] = 1);
   }
   let countArr = Object.values(count);
-  for (let i = 0; i < countArr.length; i++) {
-    for (let j = i + 1; j < countArr.length; j++) {
-      if (countArr[i] === countArr[j]) return false;
-    }
+  // for (let i = 0; i < countArr.length; i++) {
+  //   for (let j = i + 1; j < countArr.length; j++) {
+  //     if (countArr[i] === countArr[j]) return false;
+  //   }
+  // }
+  // return true;
+  let set = new Set(countArr);
+  return countArr.length === set; // cleaner solution using set found from discussion section.
+};
+
+/*
+A school is trying to take an annual photo of all the students. The students are asked to stand in a single file line in non-decreasing order by height. Let this ordering be represented by the integer array expected where expected[i] is the expected height of the ith student in line.
+
+You are given an integer array heights representing the current order that the students are standing in. Each heights[i] is the height of the ith student in line (0-indexed).
+
+Return the number of indices where heights[i] != expected[i].
+
+Input: heights = [1,1,4,2,1,3]
+Output: 3
+Explanation: 
+heights:  [1,1,4,2,1,3]
+expected: [1,1,1,2,3,4]
+Indices 2, 4, and 5 do not match.
+
+Input: heights = [5,1,2,3,4]
+Output: 5
+Explanation:
+heights:  [5,1,2,3,4]
+expected: [1,2,3,4,5]
+All indices do not match.
+
+Input: heights = [1,2,3,4,5]
+Output: 0
+Explanation:
+heights:  [1,2,3,4,5]
+expected: [1,2,3,4,5]
+All indices match.
+*/
+
+var heightChecker = function (heights) {
+  let sorted = [...heights].sort((a, b) => {
+    if (a > b) return 1;
+    else return -1;
+  });
+  let count = 0;
+  for (let i = 0; i < heights.length; i++) {
+    if (heights[i] !== sorted[i]) count += 1;
   }
-  return true;
+  return count;
 };
