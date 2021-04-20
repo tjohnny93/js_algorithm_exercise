94f9aaadbc640aff2bed831689e4f24c14a73750
Johnny Tae <tjohnny93@berkeley.edu>
2021-04-20 13:49:53 +0900

    April 20th 2 problems solved

--- april.js
+++ april.js
@@ -247,3 +247,72 @@ var heightChecker = function (heights) {
   }
   return count;
 };
+
+/*
+Given a square matrix mat, return the sum of the matrix diagonals.
+
+Only include the sum of all the elements on the primary diagonal and all the elements on the secondary diagonal that are not part of the primary diagonal.
+
+Input: mat = [[1,2,3],
+              [4,5,6],
+              [7,8,9]]
+Output: 25
+Explanation: Diagonals sum: 1 + 5 + 9 + 3 + 7 = 25
+Notice that element mat[1][1] = 5 is counted only once.
+Example 2:
+
+Input: mat = [[1,1,1,1],
+              [1,1,1,1],
+              [1,1,1,1],
+              [1,1,1,1]]
+Output: 8
+Example 3:
+
+Input: mat = [[5]]
+Output: 5
+*/
+
+var diagonalSum = function (mat) {
+  let result = 0;
+  for (let i = 0; i < mat.length; i++) {
+    let front = i;
+    let back = mat.length - 1 - front;
+    result += mat[i][front];
+    if (back !== front) result += mat[i][back];
+  }
+  return result;
+};
+
+/*
+Given the array of integers nums, you will choose two different indices i and j of that array. Return the maximum value of (nums[i]-1)*(nums[j]-1).
+ 
+
+Example 1:
+
+Input: nums = [3,4,5,2]
+Output: 12 
+Explanation: If you choose the indices i=1 and j=2 (indexed from 0), you will get the maximum value, that is, (nums[1]-1)*(nums[2]-1) = (4-1)*(5-1) = 3*4 = 12. 
+Example 2:
+
+Input: nums = [1,5,4,5]
+Output: 16
+Explanation: Choosing the indices i=1 and j=3 (indexed from 0), you will get the maximum value of (5-1)*(5-1) = 16.
+Example 3:
+
+Input: nums = [3,7]
+Output: 12
+*/
+
+var maxProduct = function (nums) {
+  let highestIdx = 0;
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] > nums[highestIdx]) highestIdx = i;
+  }
+  let result = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (highestIdx === i) continue;
+    let curr = (nums[i] - 1) * (nums[highestIdx] - 1);
+    if (curr > result) result = curr;
+  }
+  return result;
+};
