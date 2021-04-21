8954ac3160fcc315378f44a43c26550d5ebe9282
Johnny Tae <tjohnny93@berkeley.edu>
2021-04-21 16:54:26 +0900

    April 21 / 2 problems solved

--- april.js
+++ april.js
@@ -316,3 +316,83 @@ var maxProduct = function (nums) {
   }
   return result;
 };
+
+/*
+You are given an integer array nums (0-indexed). In one operation, you can choose an element of the array and increment it by 1.
+
+For example, if nums = [1,2,3], you can choose to increment nums[1] to make nums = [1,3,3].
+Return the minimum number of operations needed to make nums strictly increasing.
+
+An array nums is strictly increasing if nums[i] < nums[i+1] for all 0 <= i < nums.length - 1. An array of length 1 is trivially strictly increasing.
+
+Example 1:
+
+Input: nums = [1,1,1]
+Output: 3
+Explanation: You can do the following operations:
+1) Increment nums[2], so nums becomes [1,1,2].
+2) Increment nums[1], so nums becomes [1,2,2].
+3) Increment nums[2], so nums becomes [1,2,3].
+Example 2:
+
+Input: nums = [1,5,2,4,1]
+Output: 14
+Example 3:
+
+Input: nums = [8]
+Output: 0
+
+*/
+
+var minOperations = function (nums) {
+  let result = 0;
+  // my original solution
+  // for (let i = 0; i < nums.length; i++) {
+  //   while (nums[i] >= nums[i + 1]) {
+  //     nums[i + 1] += 1;
+  //     result += 1;
+  //   }
+  // }
+  for (let i = 0; i < nums.length - 1; i++) {
+    const diff = nums[i + 1] - nums[i];
+    if (diff <= 0) {
+      nums[i + 1] += Math.abs(diff) + 1;
+      result += Math.abs(diff) + 1;
+    }
+  }
+  return result;
+};
+
+/*
+Given an integer n, return any array containing n unique integers such that they add up to 0.
+Example 1:
+
+Input: n = 5
+Output: [-7,-1,1,3,4]
+Explanation: These arrays also are accepted [-5,-1,1,2,3] , [-3,-1,2,-2,4].
+Example 2:
+
+Input: n = 3
+Output: [-1,0,1]
+Example 3:
+
+Input: n = 1
+Output: [0]
+*/
+
+var sumZero = function (n) {
+  let result;
+  if (n % 2 === 1) {
+    result = [0];
+    for (let i = 1; result.length < n; i++) {
+      result.push(i, -i);
+    }
+  }
+  if (n % 2 === 0) {
+    result = [];
+    for (let i = 1; result.length < n; i++) {
+      result.push(i, -i);
+    }
+  }
+  return result;
+};
