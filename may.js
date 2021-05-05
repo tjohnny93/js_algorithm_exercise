b34e42550a77f08af5b4e7c66087c6589ffaeec6
Johnny Tae <tjohnny93@berkeley.edu>
2021-05-05 23:56:53 +0900

    May 5th 2 problems solved

--- may.js
+++ may.js
@@ -53,3 +53,69 @@ var commonChars = function (A) {
 
   return result;
 };
+
+/*
+The number of sandwiches in the cafeteria is equal to the number of students. The sandwiches are placed in a stack. At each step:
+
+If the student at the front of the queue prefers the sandwich on the top of the stack, they will take it and leave the queue.
+Otherwise, they will leave it and go to the queue's end.
+This continues until none of the queue students want to take the top sandwich and are thus unable to eat.
+
+You are given two integer arrays students and sandwiches where sandwiches[i] is the type of the i​​​​​​th sandwich in the stack (i = 0 is the top of the stack) and students[j] is the preference of the j​​​​​​th student in the initial queue (j = 0 is the front of the queue). Return the number of students that are unable to eat.
+
+Input: students = [1,1,0,0], sandwiches = [0,1,0,1]
+Output: 0 
+
+Input: students = [1,1,1,0,0,1], sandwiches = [1,0,0,0,1,1]
+Output: 3
+*/
+
+var countStudents = function (students, sandwiches) {
+  while (students.length > 0 && students.indexOf(sandwiches[0]) !== -1) {
+    if (students[0] === sandwiches[0]) {
+      students.shift();
+      sandwiches.shift();
+    } else {
+      students.push(students.shift());
+    }
+  }
+  return students.length;
+};
+
+/*
+Every valid email consists of a local name and a domain name, separated by the '@' sign. Besides lowercase letters, the email may contain one or more '.' or '+'.
+
+For example, in "alice@leetcode.com", "alice" is the local name, and "leetcode.com" is the domain name.
+If you add periods '.' between some characters in the local name part of an email address, mail sent there will be forwarded to the same address without dots in the local name. Note that this rule does not apply to domain names.
+
+For example, "alice.z@leetcode.com" and "alicez@leetcode.com" forward to the same email address.
+If you add a plus '+' in the local name, everything after the first plus sign will be ignored. This allows certain emails to be filtered. Note that this rule does not apply to domain names.
+
+For example, "m.y+name@email.com" will be forwarded to "my@email.com".
+It is possible to use both of these rules at the same time.
+
+Given an array of strings emails where we send one email to each email[i], return the number of different addresses that actually receive mails.
+
+Input: emails = ["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"]
+Output: 2
+Explanation: "testemail@leetcode.com" and "testemail@lee.tcode.com" actually receive mails.
+
+
+Input: emails = ["a@leetcode.com","b@leetcode.com","c@leetcode.com"]
+Output: 3
+*/
+
+var numUniqueEmails = function (emails) {
+  let result = [];
+
+  for (let i = 0; i < emails.length; i++) {
+    let parts = emails[i].split("@");
+    let local = parts[0].replace(/\./g, "").split("+")[0];
+    let completeEmail = `${local}@${parts[1]}`;
+
+    if (result.indexOf(completeEmail) === -1) {
+      result.push(completeEmail);
+    }
+  }
+  return result.length;
+};
